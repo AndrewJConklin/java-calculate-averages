7c329819432b2032465fb37b404e6225cb355a8d
aconkli9 <aconkli9@ford.com>
2022-05-26 15:19:33 -0400

    Add solution

--- App.java
+++ App.java
@@ -1,4 +1,25 @@
-class App {
+import java.util.stream.*;
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+ class App {
   public static void main(String args[]){
+    int[] scores = {72, 95, 91, 85, 87, 51, 65, 72, 75, 89};
+    Arrays.sort(scores);  
+    System.out.println(String.format("The median score is %d",scores[5]));
+    
+    int total = 0;
+    for(int i = 0; i < scores.length; i++){
+      total = total + scores[i];
+    }
+    System.out.println(String.format("The mean of all the scores is %d", (total/scores.length)));
+  
+    int max = Arrays.stream(scores).max().getAsInt();
+    System.out.println(String.format("The max of all the scores is %d", max));
+
+    int min = Arrays.stream(scores).min().getAsInt();
+    System.out.println(String.format("The max of all the scores is %d", min));
+  } 
   }
-}
+
+  
